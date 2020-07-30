fc626b356d46dea9d99446a3e22a88dd766862c8
Parineet Vernekar <parineet.vernekar@gmail.com>
2020-07-30 22:19:01 +0100

    Initial Commit

---
+++ Shared/SecondSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondSwiftUIAppApp.swift
+//  Shared
+//
+//  Created by Parineet Vernekar on 30/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SecondSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
